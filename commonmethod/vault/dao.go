94ba0195199b46b99c1726eef7e3e600e9e49db2
Giga <gidae@coinmeca.net>
2024-03-20 00:25:56 +0900

    added: recent struct

--- commonmethod/vault/dao.go
+++ commonmethod/vault/dao.go
@@ -1,1 +1,12 @@
 package vault
+
+type Recent struct {
+	Type    int     `json:"type" bson:"type"`
+	Symbol  string  `json:"symbol" bson:"symbol"`
+	Address string  `json:"address" bson:"address"`
+	Time    string  `json:"time" bson:"time"`
+	Volume  string  `json:"volume" bson:"volume"`
+	Meca    string  `json:"meca" bson:"meca"`
+	Share   float64 `json:"share" bson:"share"`
+	TxHash  string  `json:"txHash" bson:"txHash"`
+}
