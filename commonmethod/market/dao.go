4ec7779ff270d7f70ccd8607a7975f9232b651b5
Zepto <rainbowbear@coinmeca.net>
2024-04-18 02:00:05 +0900

    added: Event, method name, trade type (Sell, Buy, Ask, Bid)

--- commonmethod/market/dao.go
+++ commonmethod/market/dao.go
@@ -1,1 +1,17 @@
 package market
+
+type Recent struct {
+	Type    	int     `json:"type" bson:"type"`
+	From    	string  `json:"from" bson:"from"`
+	Address 	string  `json:"address" bson:"address"`
+	Time    	string  `json:"time" bson:"time"`
+	Price  		string  `json:"symbol" bson:"symbol"`
+	Amount  	string  `json:"amount" bson:"amount"`
+	Quantity    string  `json:"quantity" bson:"quantity"`
+	TxHash  	string  `json:"txHash" bson:"txHash"`
+}
+
+type Tick struct {
+	Price  		string  `json:"symbol" bson:"symbol"`
+	Amount  	string  `json:"amount" bson:"amount"`
+}
