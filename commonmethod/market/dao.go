a7598457dc2dd8bc292921251e8b242b6b879135
zetta <zinv@coinmeca.net>
2024-04-23 17:26:33 +0900

    Update - Market Recent Struct

--- commonmethod/market/dao.go
+++ commonmethod/market/dao.go
@@ -4,6 +4,8 @@ type Recent struct {
 	Type    	int     `json:"type" bson:"type"`
 	From    	string  `json:"from" bson:"from"`
 	Address 	string  `json:"address" bson:"address"`
+	Buy 		string  `json:"buy" bson:"buy"`
+	Sell 		string  `json:"sell" bson:"sell"`
 	Time    	string  `json:"time" bson:"time"`
 	Price  		string  `json:"symbol" bson:"symbol"`
 	Amount  	string  `json:"amount" bson:"amount"`
