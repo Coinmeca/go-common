15616685b0878cfd7ac884d3b0ca0ea5b12be356
zetta <zinv@coinmeca.net>
2024-04-28 00:19:24 +0900

    Update - Farm Recent

--- commonmethod/farm/dao.go
+++ commonmethod/farm/dao.go
@@ -6,5 +6,6 @@ type Recent struct {
 	Farm		string	`json:"farm" bson:"farm"`
 	User    	string  `json:"owner" bson:"owner"`
 	Amount		string	`json:"amount" bson:"amount"`
+	Share		string	`json:"share" bson:"share"`
 	TxHash  	string  `json:"txHash" bson:"txHash"`
 }
