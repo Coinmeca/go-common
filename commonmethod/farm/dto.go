28248af5e7e77524650ea696727c37997fa19982
Giga <gidae@coinmeca.net>
2024-04-28 15:09:13 +0900

    Fix: farm struct

--- commonmethod/farm/dto.go
+++ commonmethod/farm/dto.go
@@ -1,1 +1,8 @@
 package farm
+
+type FarmTokenInfo struct {
+	Decimals int    `json:"decimals" bson:"decimals"`
+	Address  string `json:"address" bson:"address"`
+	Symbol   string `json:"symbol" bson:"symbol"`
+	Name     string `json:"name" bson:"name"`
+}
