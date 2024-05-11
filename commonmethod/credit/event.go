b0f5d3796a0d09e4d2ccc8c20fa8399b929ab9c4
Giga <gidae@coinmeca.net>
2024-05-12 00:07:53 +0900

    Add: credit event name

---
+++ commonmethod/credit/event.go
@@ -0,0 +1,17 @@
+package credit
+
+const (
+	EventSetUserName = "SetUserName"
+	EventSetUserImg  = "SetUserImg"
+	EventSetUser     = "SetUser"
+	EventAppAdd      = "AppAdd"
+	EventAppRemove   = "AppRemove"
+
+	EventIncreasePoint = "IncreasePoint"
+	EventDecreasePoint = "DecreasePoint"
+	EventIncreaseScore = "IncreaseScore"
+	EventDecreaseScore = "DecreaseScore"
+
+	EventSetPoint = "SetPoint"
+	EventSetScore = "SetScore"
+)
