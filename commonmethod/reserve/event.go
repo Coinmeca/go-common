eb9de8c3692db5d66c6885d842b11f8b9fc9c865
Giga <gidae@coinmeca.net>
2024-05-12 01:15:52 +0900

    Add: const reserve event name

---
+++ commonmethod/reserve/event.go
@@ -0,0 +1,10 @@
+package reserve
+
+const (
+	EventNameLockup     = "Lockup"
+	EventNameUnlock     = "Unlock"
+	EventNameDistribute = "Distribute"
+	EventNameResume     = "Resume"
+	EventNameTerminate  = "Terminate"
+	EventNamePermission = "Permission"
+)
