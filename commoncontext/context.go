95b7a9807572e2ce558508d763f05042b0398975
Giga <gidae@coinmeca.net>
2024-05-18 02:59:42 +0900

    Add: custom common context

--- commoncontext/context.go
+++ commoncontext/context.go
@@ -1,1 +1,31 @@
 package commoncontext
+
+import "context"
+
+type Context interface {
+	context.Context
+	ChainId() string
+	UserId() string
+}
+
+type commonContext struct {
+	context.Context
+	chainId string
+	userId  string
+}
+
+func (c *commonContext) ChainId() string {
+	return c.chainId
+}
+
+func (c *commonContext) UserId() string {
+	return c.userId
+}
+
+func NewCommonContext(ctx context.Context, chainId, userId string) Context {
+	return &commonContext{
+		Context: ctx,
+		chainId: chainId,
+		userId:  userId,
+	}
+}
