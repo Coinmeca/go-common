ebbaf755bf4025e976b4a3029c497a332f216d9f
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-06-17 08:35:44 +0900

    Update - Vault

--- commonmethod/vault/dao.go
+++ commonmethod/vault/dao.go
@@ -5,80 +5,81 @@ import (
 )
 
 type Recent struct {
-	ChainId				string					`json:"chainId" bson:"chainId"`
-	Address				string					`json:"address" bson:"address"`
-	Time				int64					`json:"time" bson:"time"`
-	Type				int64					`json:"type" bson:"type"`
-	User				string					`json:"user" bson:"user"`
-	Token				string					`json:"token" bson:"token"`
-	Amount				primitive.Decimal128	`json:"volume" bson:"volume"`
-	Meca				primitive.Decimal128	`json:"meca" bson:"meca"`
-	Share				primitive.Decimal128	`json:"share" bson:"share"`
-	TxHash				string					`json:"txHash" bson:"txHash"`
-	UpdateAt			string					`json:"updateAt" bson:"updateAt"`
+	ChainId  string               `json:"chainId" bson:"chainId"`
+	Address  string               `json:"address" bson:"address"`
+	Time     int64                `json:"time" bson:"time"`
+	Type     int64                `json:"type" bson:"type"`
+	User     string               `json:"user" bson:"user"`
+	Token    string               `json:"token" bson:"token"`
+	Amount   primitive.Decimal128 `json:"volume" bson:"volume"`
+	Meca     primitive.Decimal128 `json:"meca" bson:"meca"`
+	Share    primitive.Decimal128 `json:"share" bson:"share"`
+	TxHash   string               `json:"txHash" bson:"txHash"`
+	UpdateAt string               `json:"updateAt" bson:"updateAt"`
 }
 
 type Last struct {
-	ChainId				string					`json:"chainId" bson:"chainId"`
-	Address				string					`json:"address" bson:"address"`
-	Exchange			primitive.Decimal128	`json:"exchange" bson:"exchange"`
-	Deposit				primitive.Decimal128	`json:"deposit" bson:"deposit"`
-	Withdraw			primitive.Decimal128	`json:"withdraw" bson:"withdraw"`
-	Locked				primitive.Decimal128	`json:"locked" bson:"locked"`
-	ValueLocked			primitive.Decimal128	`json:"valueLocked" bson:"valueLocked"`
-	Value				primitive.Decimal128	`json:"value" bson:"value"`
-	Weight				primitive.Decimal128	`json:"weight" bson:"weight"`
-	Mint				primitive.Decimal128	`json:"mint" bson:"mint"`
-	Burn				primitive.Decimal128	`json:"burn" bson:"burn"`
-	Chart				Chart					`json:"chart" bson:"chart"`
-	ChartSub			ChartSub				`json:"chartSub" bson:"chartSub"`
-	Recent				Recent					`json:"recent" bson:"Recent"`
+	ChainId     string               `json:"chainId" bson:"chainId"`
+	Address     string               `json:"address" bson:"address"`
+	Exchange    primitive.Decimal128 `json:"exchange" bson:"exchange"`
+	Deposit     primitive.Decimal128 `json:"deposit" bson:"deposit"`
+	Withdraw    primitive.Decimal128 `json:"withdraw" bson:"withdraw"`
+	Locked      primitive.Decimal128 `json:"locked" bson:"locked"`
+	ValueLocked primitive.Decimal128 `json:"valueLocked" bson:"valueLocked"`
+	Value       primitive.Decimal128 `json:"value" bson:"value"`
+	Weight      primitive.Decimal128 `json:"weight" bson:"weight"`
+	Mint        primitive.Decimal128 `json:"mint" bson:"mint"`
+	Burn        primitive.Decimal128 `json:"burn" bson:"burn"`
+	Chart       Chart                `json:"chart" bson:"chart"`
+	ChartSub    ChartSub             `json:"chartSub" bson:"chartSub"`
+	Recent      Recent               `json:"recent" bson:"Recent"`
 }
 
 type Vault struct {
-	ChainId				string					`json:"chainId" bson:"chainId"`
-	Address				string					`json:"address" bson:"address"`
-	Key					bool					`json:"key" bson:"key"`
-	Name				string					`json:"name" bson:"name"`
-	Symbol				string					`json:"symbol" bson:"symbol"`
-	Decimals			int64					`json:"decimals" bson:"decimals"`
-	Exchange			primitive.Decimal128	`json:"exchange" bson:"exchange"`
-	Rate				primitive.Decimal128	`json:"rate" bson:"rate"`
-	Locked				primitive.Decimal128	`json:"locked" bson:"locked"`
-	ValueLocked			primitive.Decimal128	`json:"valueLocked" bson:"valueLocked"`
-	Weight				primitive.Decimal128	`json:"weight" bson:"weight"`
-	Deposit				primitive.Decimal128	`json:"deposit" bson:"deposit"`
-	Deposit24h			primitive.Decimal128	`json:"deposit24h" bson:"deposit24h"`
-	Withdraw			primitive.Decimal128	`json:"withdraw" bson:"withdraw"`
-	Withdraw24h			primitive.Decimal128	`json:"withdraw24h" bson:"withdraw24h"`
-	PerToken			primitive.Decimal128	`json:"perToken" bson:"perToken"`
-	TokenPer			primitive.Decimal128	`json:"tokenPer" bson:"tokenPer"`
-	Mint				primitive.Decimal128	`json:"mint" bson:"mint"`
-	Burn				primitive.Decimal128	`json:"burn" bson:"burn"`
-	Value				primitive.Decimal128	`json:"value" bson:"value"`
-	Chart				[]Chart					`json:"chart" bson:"chart"`
-	ChartSub			[]ChartSub				`json:"chartSub" bson:"chartSub"`
-	Recents				[]Recent				`json:"recents" bson:"recents"`
-	Last				Last					`json:"last" bson:"last"`
+	ChainId     string               `json:"chainId" bson:"chainId"`
+	Address     string               `json:"address" bson:"address"`
+	Key         bool                 `json:"key" bson:"key"`
+	Name        string               `json:"name" bson:"name"`
+	Symbol      string               `json:"symbol" bson:"symbol"`
+	Decimals    int64                `json:"decimals" bson:"decimals"`
+	Exchange    primitive.Decimal128 `json:"exchange" bson:"exchange"`
+	Rate        primitive.Decimal128 `json:"rate" bson:"rate"`
+	Locked      primitive.Decimal128 `json:"locked" bson:"locked"`
+	ValueLocked primitive.Decimal128 `json:"valueLocked" bson:"valueLocked"`
+	Weight      primitive.Decimal128 `json:"weight" bson:"weight"`
+	Need        primitive.Decimal128 `json:"need" bson:"need"`
+	Deposit     primitive.Decimal128 `json:"deposit" bson:"deposit"`
+	Deposit24h  primitive.Decimal128 `json:"deposit24h" bson:"deposit24h"`
+	Withdraw    primitive.Decimal128 `json:"withdraw" bson:"withdraw"`
+	Withdraw24h primitive.Decimal128 `json:"withdraw24h" bson:"withdraw24h"`
+	PerToken    primitive.Decimal128 `json:"perToken" bson:"perToken"`
+	TokenPer    primitive.Decimal128 `json:"tokenPer" bson:"tokenPer"`
+	Mint        primitive.Decimal128 `json:"mint" bson:"mint"`
+	Burn        primitive.Decimal128 `json:"burn" bson:"burn"`
+	Value       primitive.Decimal128 `json:"value" bson:"value"`
+	Chart       []Chart              `json:"chart" bson:"chart"`
+	ChartSub    []ChartSub           `json:"chartSub" bson:"chartSub"`
+	Recents     []Recent             `json:"recents" bson:"recents"`
+	Last        Last                 `json:"last" bson:"last"`
 }
 
 type Chart struct {
-	ChainId				string					`json:"chainId" bson:"chainId"`
-	Address				string					`json:"market" bson:"market"`
-	Time				int64					`json:"time" bson:"time"`
-	Open				primitive.Decimal128	`json:"open" bson:"open"`
-	High				primitive.Decimal128	`json:"high" bson:"high"`
-	Low					primitive.Decimal128	`json:"low" bson:"low"`
-	Close				primitive.Decimal128	`json:"close" bson:"close"`
-	Volume				primitive.Decimal128	`json:"volume" bson:"volume"`
+	ChainId string               `json:"chainId" bson:"chainId"`
+	Address string               `json:"market" bson:"market"`
+	Time    int64                `json:"time" bson:"time"`
+	Open    primitive.Decimal128 `json:"open" bson:"open"`
+	High    primitive.Decimal128 `json:"high" bson:"high"`
+	Low     primitive.Decimal128 `json:"low" bson:"low"`
+	Close   primitive.Decimal128 `json:"close" bson:"close"`
+	Volume  primitive.Decimal128 `json:"volume" bson:"volume"`
 }
 
 type ChartSub struct {
-	ChainId				string					`json:"chainId" bson:"chainId"`
-	Address				string					`json:"address" bson:"address"`
-	Time				int64					`json:"time" bson:"time"`
-	Weight				primitive.Decimal128	`json:"weight" bson:"weight"`
-	Locked				primitive.Decimal128	`json:"locked" bson:"locked"`
-	Value				primitive.Decimal128	`json:"value" bson:"value"`
-	ValueLocked			primitive.Decimal128	`json:"ValueLocked" bson:"ValueLocked"`
+	ChainId     string               `json:"chainId" bson:"chainId"`
+	Address     string               `json:"address" bson:"address"`
+	Time        int64                `json:"time" bson:"time"`
+	Weight      primitive.Decimal128 `json:"weight" bson:"weight"`
+	Locked      primitive.Decimal128 `json:"locked" bson:"locked"`
+	Value       primitive.Decimal128 `json:"value" bson:"value"`
+	ValueLocked primitive.Decimal128 `json:"ValueLocked" bson:"ValueLocked"`
 }
