c9cbe0a30afbab8d5735ac946f1a44d2957bdb58
Giga <gidae@coinmeca.net>
2024-07-14 05:01:30 +0900

    Fix: log file save

--- commonlog/logger.go
+++ commonlog/logger.go
@@ -27,14 +27,14 @@ func InitLog(filePath string, useConsole bool) {
 		cores = append(cores, consoleCore)
 	}
 
-	//fileEncoderConfig := zap.NewProductionEncoderConfig()
-	//fileEncoder := zapcore.NewJSONEncoder(fileEncoderConfig)
-	//logFile, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	//if err != nil {
-	//	panic("Failed to open log file: " + err.Error())
-	//}
-	//fileCore := zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), zapcore.DebugLevel)
-	//cores = append(cores, fileCore)
+	fileEncoderConfig := zap.NewProductionEncoderConfig()
+	fileEncoder := zapcore.NewJSONEncoder(fileEncoderConfig)
+	logFile, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic("Failed to open log file: " + err.Error())
+	}
+	fileCore := zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), zapcore.DebugLevel)
+	cores = append(cores, fileCore)
 
 	combinedCore := zapcore.NewTee(cores...)
 	Logger = zap.New(combinedCore)
