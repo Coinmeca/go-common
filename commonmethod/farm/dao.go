cf9a89d25c1a251fde058da3e0f06d5ec3f1b997
Giga <gidae@coinmeca.net>
2024-07-28 03:42:26 +0900

    Fix: farm struct staking, unstaking change

--- commonmethod/farm/dao.go
+++ commonmethod/farm/dao.go
@@ -56,29 +56,31 @@ type Last struct {
 }
 
 type Farm struct {
-	ChainId      string               `json:"chainId" bson:"chainId"`
-	Address      string               `json:"address" bson:"address"`
-	Type         bool                 `json:"type" bson:"type"`
-	Id           int64                `json:"id" bson:"id"`
-	Master       string               `json:"master" bson:"master"`
-	Name         string               `json:"name" bson:"name"`
-	Symbol       string               `json:"symbol" bson:"symbol"`
-	Decimals     int64                `json:"decimals" bson:"decimals"`
-	Stake        token.Token          `json:"stake" bson:"stake"`
-	Earn         token.Token          `json:"earn" bson:"earn"`
-	Start        int64                `json:"start" bson:"start"`
-	Period       int64                `json:"period" bson:"period"`
-	Duration     int64                `json:"duration" bson:"duration"`
-	Goal         int64                `json:"goal" bson:"goal"`
-	Staked       primitive.Decimal128 `json:"staked" bson:"staked"`
-	Interest     primitive.Decimal128 `json:"interest" bson:"interest"`
-	Staking24h   primitive.Decimal128 `json:"staking24h" bson:"staking24h"`
-	Unstaking24h primitive.Decimal128 `json:"unstaking24h" bson:"unstaking24h"`
-	Interest24h  primitive.Decimal128 `json:"interest24h" bson:"interest24h"`
-	ValueStaked  primitive.Decimal128 `json:"valueLocked" bson:"valueLocked"`
-	Total        primitive.Decimal128 `json:"total" bson:"total"`
-	Apr          primitive.Decimal128 `json:"apr" bson:"apr"`
-	Chart        []Chart              `json:"chart" bson:"chart"`
-	Recents      []Recent             `json:"recents" bson:"recents"`
-	Last         Last                 `json:"last" bson:"bson"`
+	ChainId            string               `json:"chainId" bson:"chainId"`
+	Address            string               `json:"address" bson:"address"`
+	Type               bool                 `json:"type" bson:"type"`
+	Id                 int64                `json:"id" bson:"id"`
+	Master             string               `json:"master" bson:"master"`
+	Name               string               `json:"name" bson:"name"`
+	Symbol             string               `json:"symbol" bson:"symbol"`
+	Decimals           int64                `json:"decimals" bson:"decimals"`
+	Stake              token.Token          `json:"stake" bson:"stake"`
+	Earn               token.Token          `json:"earn" bson:"earn"`
+	Start              int64                `json:"start" bson:"start"`
+	Period             int64                `json:"period" bson:"period"`
+	Duration           int64                `json:"duration" bson:"duration"`
+	Goal               int64                `json:"goal" bson:"goal"`
+	Staked             primitive.Decimal128 `json:"staked" bson:"staked"`
+	Interest           primitive.Decimal128 `json:"interest" bson:"interest"`
+	Staking24h         primitive.Decimal128 `json:"staking24h" bson:"staking24h"`
+	Staking24hChange   primitive.Decimal128 `json:"staking24hChange" bson:"staking24hChange"`
+	UnStaking24h       primitive.Decimal128 `json:"unStaking24h" bson:"unStaking24h"`
+	UnStaking24hChange primitive.Decimal128 `json:"unStaking24hChange" bson:"unStaking24hChange"`
+	Interest24h        primitive.Decimal128 `json:"interest24h" bson:"interest24h"`
+	ValueStaked        primitive.Decimal128 `json:"valueLocked" bson:"valueLocked"`
+	Total              primitive.Decimal128 `json:"total" bson:"total"`
+	Apr                primitive.Decimal128 `json:"apr" bson:"apr"`
+	Chart              []Chart              `json:"chart" bson:"chart"`
+	Recents            []Recent             `json:"recents" bson:"recents"`
+	Last               Last                 `json:"last" bson:"bson"`
 }
