d6bef62c711d9c909f9988095b29c71afdd4b999
Giga <gidae@coinmeca.net>
2024-07-29 05:37:47 +0900

    Fix: token struct value

--- commonmethod/token/dao.go
+++ commonmethod/token/dao.go
@@ -1,8 +1,11 @@
 package token
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Token struct {
-	Address		string					`json:"address" bson:"address"`
-	Name		string					`json:"name" bson:"name"`
-	Symbol		string					`json:"symbol" bson:"symbol"`
-	Decimals	int64					`json:"decimals" bson:"decimals"`
+	Address  string               `json:"address" bson:"address"`
+	Name     string               `json:"name" bson:"name"`
+	Symbol   string               `json:"symbol" bson:"symbol"`
+	Decimals int64                `json:"decimals" bson:"decimals"`
+	Value    primitive.Decimal128 `json:"value" bson:"value"`
 }
