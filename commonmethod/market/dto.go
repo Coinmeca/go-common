52ed089f9e49652b54e4a3b8998388715ee2c695
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-09-07 06:45:17 +0900

    Add - Market Liquidity DTO

--- commonmethod/market/dto.go
+++ commonmethod/market/dto.go
@@ -1,9 +1,16 @@
 package market
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type MarketTokenInfo struct {
-	Name				string			`json:"name" bson:"name"`
-	Symbol				string			`json:"symbol" bson:"symbol"`
-	Decimals			int				`json:"decimals" bson:"decimals"`
-	Address				string			`json:"address" bson:"address"`
-	Liquidity			float64			`json:"liquidity" bson:"liquidity"`
+	Name      string  `json:"name" bson:"name"`
+	Symbol    string  `json:"symbol" bson:"symbol"`
+	Decimals  int     `json:"decimals" bson:"decimals"`
+	Address   string  `json:"address" bson:"address"`
+	Liquidity float64 `json:"liquidity" bson:"liquidity"`
+}
+
+type MarketLiquidity struct {
+	Type   bool
+	Amount *primitive.Decimal128
 }
